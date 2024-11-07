31c43c5d6be5a28343a3213be1254cb26f185c3c
Floyd_Carlo <codeit69420@gmail.com>
2024-11-07 08:17:20 +0530

    Update 9.py

--- OOPS/9.py
+++ OOPS/9.py
@@ -1,6 +1,8 @@
-#Write a program to create a class called Shape with methods called getPerimeter() and
+"""
+Write a program to create a class called Shape with methods called getPerimeter() and
 getArea(). Create a subclass called Circle that overrides the getPerimeter() and getArea() 
 methods to calculate the area and perimeter of a circle.(use abstract classes and methods)
+"""
 
 from abc import ABC, abstractmethod
 import math
